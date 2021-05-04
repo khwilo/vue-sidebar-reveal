b63beddbba59bf2eed16b3329cba9d6639f7073d
Khwilo Kabaka <khwilowatai@gmail.com>
2021-05-05 00:18:57 +0300

    Install and set up font awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCog, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+library.add(faUser, faCog);
+
+createApp(App).component('FontAwesomeIcon', FontAwesomeIcon).use(router).mount('#app');
